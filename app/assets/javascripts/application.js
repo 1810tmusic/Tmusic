7759ced779d92919ac7ed6dde54e9cd0c76fbf32
磯崎裕太 <yutaisozaki@isozakiyuutanoMacBook-puro.local>
2018-11-19 12:06:58 +0900

    Cart3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,13 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+$(function() {
+	$(document).on("ajax:success", ".fav", function(e) {
+		if ($('#' + e.detail[0]).hasClass('fa-heart')) {
+			$('#' + e.detail[0]).removeClass('fa-heart').addClass('fa-heart-o');
+		} else {
+			$('#' + e.detail[0]).removeClass('fa-heart-o').addClass('fa-heart');
+		}
+	})
+})
