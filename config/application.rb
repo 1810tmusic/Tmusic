b21dc294dfd5b13578cf749376dfad1558ca2ba3
Erika <Erika@Erika-no-MacBook-Air.local>
2018-11-27 17:00:28 +0900

    Revert "change user's info"
    This reverts commit 5c250e3f3e4e4667040f45ac3acec4ff735e5dc4.

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,5 @@ module Tmusic
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.autoload_paths << Rails.root.join('lib')
-    config.i18n.default_locale = :ja
-
   end
 end
