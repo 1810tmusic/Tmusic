541da6693ca72df469801445555bd1515f3e6482
磯崎裕太 <yutaisozaki@isozakiyuutanoMacBook-puro.local>
2018-11-28 20:21:00 +0900

    heroku

--- db/migrate/20181108055656_add_details_to_users.rb
+++ db/migrate/20181108055656_add_details_to_users.rb
@@ -9,13 +9,6 @@ class AddDetailsToUsers < ActiveRecord::Migration[5.2]
     add_column :users, :nickname, :string
     add_column :users, :birthday, :date
     add_column :users, :leave_at, :datetime
-    change_column :users, :name, :string, null: false
-    change_column :users, :name_kana, :string, null: false
-    change_column :users, :postal_code, :string, null: false
-    change_column :users, :address, :string, null: false
-    change_column :users, :phone_number, :string, null: false
-    change_column :users, :nickname, :string, null: false
-    change_column :users, :birthday, :date, null: false
   end
 
   def down
